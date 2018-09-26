7aa1218436b2379fe8e1aad61099622172ce5f47
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2018-09-27 01:41:04 +1000

    game methods implementing

--- DistributedScrabbleGame/Server/src/Cell.java
+++ DistributedScrabbleGame/Server/src/Cell.java
@@ -2,11 +2,26 @@ public class Cell {
 
     private char letter;
 
-
     /**
      * Constructor
      */
     public Cell() {
 
+    }
+
+
+    /**
+     * Mutator
+     */
+    private void setLetter(char letter) {
+        this.letter = letter;
+    }
+
+    /**
+     * Accessor
+     * @return current letter in the cell
+     */
+    private char getLetter() {
+        return letter;
     }
 }
